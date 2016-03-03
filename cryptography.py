357e72282551f9d4fdf43b9a9e959561d5a0d26a
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-03-03 14:32:39 -0500

    Updated from Brython Server: 3/3/2016 2:32:41 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Daniel Wilson
+Credit: Avery
 
 Assignment:
 
@@ -10,3 +10,9 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+let = []
+other = []
+
+id=input("Enter e to encrypt, d to decrypt, or q to quit")
+if id = "e":
+    
