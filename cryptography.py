db8dc940d5cf236540d0dddce627eebbaeab4f5f
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-03-04 14:43:26 -0500

    Updated from Brython Server: 3/4/2016 2:43:27 PM

--- cryptography.py
+++ cryptography.py
@@ -51,19 +51,18 @@ id=input("Enter e to encrypt, d to decrypt, or q to quit: ")
 if id not in('e', 'd', 'q'):
     print("Did not understand command, try again")
     id=input("Enter e to encrypt, d to decrypt, or q to quit: ")
-
-if id == "e":
-    message=str(input("Message: "))
-    key=input("Key: ")
-    l=len(message)
-    k=len(key)
-    for x in range(0, l):
-        let.apend(associations.find(message[x])
-    for y in range(0, k):
-        let.apend(associations.find(message[y])
-elif id=="d" :
-    dcrpt=input("Message: ")
-    key=str(input("Key: "))
-elif id == "q" :
-    print("Goodbye!")
+    if id == "e":
+        message=str(input("Message: "))
+        key=input("Key: ")
+        l=len(message)
+        k=len(key)
+        for x in range(0, l):
+            let.apend(associations.find(message[x])
+        for y in range(0, k):
+            let.apend(associations.find(message[y])
+    elif id=="d" :
+        dcrpt=input("Message: ")
+        key=str(input("Key: "))
+    elif id == "q" :
+        print("Goodbye!")
 '''
