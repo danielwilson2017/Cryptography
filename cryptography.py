2820c757e3a088537895944afe5e3be28a057cdf
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-03-08 14:32:30 -0500

    Updated from Brython Server: 3/8/2016 2:32:33 PM

--- cryptography.py
+++ cryptography.py
@@ -50,8 +50,40 @@ def encrypt(P):
     print("")
 
 def decrypt(Z):
-    key=input("Key: ")
-    Z=list(Z)
+   key=input("Key: ")
+    P=list(P)
+    z=0
+    p=[]
+    for x in P:
+        p.append(associations.index(x))
+    #print(p)
+    K=[]
+    for x in key:
+        K.append(associations.index(x))
+        z=z+1
+    #print(c)
+    c=[]
+    q=0
+    while q<len(p):
+        c.append((p[q]-K[q%len(K)])%len(associations))
+        q=q+1
+    #print(c)
+    z=0
+    C=[]
+    while z<len(p):
+        C.append(associations[c[z]])
+        z=z+1
+    #print(C)
+    s=""
+    
+    for x in C:
+        s=s+x
+    print(s)
+    return(s)
+    
+    for x in C:
+        print(x,end="")
+    print("")
     
 
 def request_answer():
